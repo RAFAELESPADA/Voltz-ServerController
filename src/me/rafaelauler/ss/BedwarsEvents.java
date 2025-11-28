7ade223e104e2e483607a9484cee244e65a9598c
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2025-11-27 23:57:03 -0300

    Update BedwarsEvents.java

--- src/me/rafaelauler/ss/BedwarsEvents.java
+++ src/me/rafaelauler/ss/BedwarsEvents.java
@@ -1,5 +1,96 @@
 package me.rafaelauler.ss;
 
-public class BedwarsEvents {
 
-}
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.jetbrains.annotations.NotNull;
+
+import com.andrei1058.bedwars.api.BedWars;
+
+import br.com.ystoreplugins.lib.jda.net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.model.user.UserManager;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import net.wavemc.core.bukkit.WaveBukkit;
+import net.wavemc.core.bukkit.account.WavePlayer;
+
+
+	public class BedwarsEvents implements Listener {
+	    private final BukkitMain plugin;
+	    private final LuckPerms luckPerms;
+	    public static Economy econ = null;
+	    public static Permission perms = null;
+	    public BedwarsEvents(BukkitMain plugin, LuckPerms luckPerms) {
+	        this.plugin = plugin;
+	        this.luckPerms = luckPerms;
+	    }
+	    BedWars bedwarsAPI = Bukkit.getServicesManager().getRegistration(BedWars.class).getProvider();
+	    
+	    public void onSlashCommandInteraction(SlashCommandInteractionEvent arg0)
+	    {
+	        // Only accept commands from guilds
+	        if (arg0.getGuild() == null)
+	            return;
+	        switch (arg0.getName())
+	        {
+	        
+	        case "status":
+	          	 try {
+	               aa(arg0, arg0.getOption("nick").getAsString()); // content is required so no null-check here
+	               break;
+	          	 }
+	          	 catch (Exception e) {
+	          	 	e.printStackTrace();
+	          	 }
+	         default:
+	            arg0.reply("I can't handle that command right now :(").setEphemeral(true).queue();
+	    }
+	        }
+		protected boolean aa(@NotNull SlashCommandInteractionEvent arg0 , String nick) {
+		   WavePlayer p = WaveBukkit.getInstance().getPlayerManager().getPlayer(nick);
+	       RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+			if (provider != null) {
+	OfflinePlayer real = Bukkit.getOfflinePlayer(nick);
+			    LuckPerms api = provider.getProvider();
+				
+		   try {
+
+		   arg0.reply("**INFORMAÇÕES DE ESTATÍSTICAS DO JOGADOR **" + p.getName() + "\nKills ( KITPVP ): " + p.getPvp().getKills() + "\nMortes: " + p.getPvp().getDeaths() + "\nKillStreak atual ( KITPVP ): " + p.getPvp().getKillstreak() + "\nKills na FPS ( KITPVP ): " + p.getPvp().getKillsfps() + "\nWins ( Sumô ): " + p.getPvp().getWinssumo() + "\nWins no duelos (1v1): " + p.getPvp().getWinsx1() + "\nXP: " + p.getPvp().getXp() + "\nCoins: " + p.getPvp().getCoins() + "\nMortes no 1v1: " + p.getPvp().getDeathsx1() + "\nWinStreak no 1v1: " + p.getPvp().getWinstreakx1() + "\nCargo: " + giveMeADamnUser(real.getUniqueId()).getPrimaryGroup().toUpperCase().toString() + "\nKills ( Bedwars ): " + bedwarsAPI.getStatsUtil().getPlayerKills(real.getUniqueId()) + "\nMortes ( Bedwars ): " + bedwarsAPI.getStatsUtil().getPlayerDeaths(real.getUniqueId()) + "\nWins ( Bedwars ): " + bedwarsAPI.getStatsUtil().getPlayerWins(real.getUniqueId()) + "\nCamas Destrúidas ( Bedwars ): " + bedwarsAPI.getStatsUtil().getPlayerBedsDestroyed(real.getUniqueId()) + "\nPerdas ( Bedwars ): " + bedwarsAPI.getStatsUtil().getPlayerLoses(real.getUniqueId()) + "\nPartidas Jogadas (BedWars): " + bedwarsAPI.getStatsUtil().getPlayerGamesPlayed(real.getUniqueId()) + "\n Nível ( Bedwars ): " + bedwarsAPI.getLevelsUtil().getCurrentXp(real.getPlayer())).queue();
+		   return true; 
+		   }
+		   catch (Exception e) {
+			   arg0.reply("Um erro ocorreu").setEphemeral(true).queue();
+			   e.printStackTrace();
+			   }
+		
+	}
+			return false;
+		}
+		public User giveMeADamnUser(UUID uniqueId) {
+			 RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+				if (provider == null) return null; 
+				    LuckPerms api = provider.getProvider();
+		    UserManager userManager = api.getUserManager();
+		    CompletableFuture<User> userFuture = userManager.loadUser(uniqueId);
+
+		    return userFuture.join(); 
+				}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+	       
+	}
+	}
+	
