0ca7bb676cce59fe90c70aba1c6d8bb9ed95b491
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2026-02-17 13:56:06 -0300

    Update PlayerTellEvent.java

--- src/me/rafaelauler/ss/PlayerTellEvent.java
+++ src/me/rafaelauler/ss/PlayerTellEvent.java
@@ -1,20 +1,27 @@
 package me.rafaelauler.ss;
 
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Cancellable;
 import net.md_5.bungee.api.plugin.Event;
 
 public class PlayerTellEvent extends Event implements Cancellable {
     private String message;
     private boolean isCancelled;
+    private ProxiedPlayer player;
 
-    public PlayerTellEvent(String message){
+    public PlayerTellEvent(String message, ProxiedPlayer player){
         this.message = message;
         this.isCancelled = false;
+        this.player = player;
     }
 
     @Override
     public boolean isCancelled() {
         return isCancelled;
+    }
+    
+    public ProxiedPlayer getPlayer() {
+        return player;
     }
 
     @Override
