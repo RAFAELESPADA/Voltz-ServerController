4e5b73a87fed3d29191c6967d193dd8c73b22b49
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2026-02-17 13:56:43 -0300

    Update ReplyCommand.java

--- src/me/rafaelauler/ss/ReplyCommand.java
+++ src/me/rafaelauler/ss/ReplyCommand.java
@@ -1,45 +1,41 @@
 package me.rafaelauler.ss;
 
-import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.Cancellable;
+import net.md_5.bungee.api.plugin.Event;
 
-public class ReplyCommand extends Command {
-  private final TellCommand tellCommand;
-  
-  public ReplyCommand(TellCommand tellCommand) {
-    super("r", null, new String[] { "reply", "responder" });
-    this.tellCommand = tellCommand;
-  }
-  
-  public void execute(CommandSender sender, String[] args) {
-    if (!(sender instanceof ProxiedPlayer)) {
-      sender.sendMessage("§cEsse comando só pode ser executado por jogadores.");
-      return;
-    } 
-    ProxiedPlayer player = (ProxiedPlayer)sender;
-    ProxiedPlayer lastRecipient = TellCommand.getLastMessageRecipient(player);
-    if (lastRecipient == null) {
-      sender.sendMessage("§cVocê não tem jogadores para responder.");
-      return;
-    } 
-    if (args.length == 0) {
-      sender.sendMessage("§cComando incorreto. /r <mensagem>");
-      return;
-    } 
-    
-    String message = String.join(" ", (CharSequence[])args);
-    PlayerTellEvent event = new PlayerTellEvent(message);
-    ProxyServer.getInstance().getPluginManager().callEvent(event);
+public class PlayerTellEvent extends Event implements Cancellable {
+    private String message;
+    private boolean isCancelled;
+    private ProxiedPlayer player;
 
-    if(event.isCancelled()){
-    	sender.sendMessage(TextComponent.fromLegacyText("§cSeu acesso ao recurso de mensagens privadas está restrito por causa de punições ativas em sua conta"));
-        return;
+    public PlayerTellEvent(String message, ProxiedPlayer player){
+        this.message = message;
+        this.isCancelled = false;
+        this.player = player;
     }
-    lastRecipient.sendMessage(TextComponent.fromLegacyText("§c§lTELL RECEBIDO §7" + player.getName() + ": §f" + message));
-    sender.sendMessage(TextComponent.fromLegacyText("§c§lTELL ENVIADO §7" + lastRecipient.getName() + ": §f" + message));
-  }
-}
 
+    @Override
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+    
+    public ProxiedPlayer getPlayer() {
+        return player;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.isCancelled = cancelled;
+    }
+
+   
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
